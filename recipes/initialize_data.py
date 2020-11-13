43abbf38be589399015b3ad07247045d4fd9adff
JohnnyBoomKek <summer1710@qq.com>
2020-11-13 11:48:26 +0800

    minor

--- recipes/initialize_data.py
+++ recipes/initialize_data.py
@@ -1,20 +1,13 @@
 import csv 
 from  recipes.models  import Ingredient, Tag
 
-def fill_ingredients(Ingredient):
-    with open('./recipes/ingredients.csv', newline='', encoding='utf-8') as File:
-        reader = csv.reader(File)
-        for row in reader:
-            ingred = Ingredient(title=row[0], dimension=row[1])
-            ingred.save()
+with open('./recipes/ingredients.csv', newline='', encoding='utf-8') as File:
+    reader = csv.reader(File)
+    for row in reader:
+        ingred = Ingredient(title=row[0], dimension=row[1])
+        ingred.save()
 
-def fill_tags(Tag):
-    tags = ['B', 'L', 'D']
-    for tag in tags:
-        new_tag = Tag(tag_name=tag)
-        new_tag.save()
-
-
-   
-fill_ingredients(Ingredient)
-fill_tags(Tag)
+tags = ['B', 'L', 'D']
+for tag in tags:
+    new_tag = Tag(tag_name=tag)
+    new_tag.save()
